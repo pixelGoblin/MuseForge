2e636dc8455022a727e810042ec16816fdc84954
Sartre666 <delphi100500@gmail.com>
2025-03-15 22:24:44 +0300

    feat: add empty sidebar template

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,36 @@
 <script setup>
 import KeywordFields from './components/KeywordFields.vue'
 import InspirationCards from './components/InspirationCards.vue'
+import Sidebar from './components/Sidebar.vue'
 </script>
 
 <template>
   <header>
-
+    <span class="title">
+      MuseForge — Generate project ideas
+    </span>
+    
   </header>
 
   <main>
-    <KeywordFields />
+    <div class="main-content">
+      <KeywordFields />
+      <InspirationCards />
+    </div>
+    <aside>
+      <Sidebar /> 
+    </aside>
   </main>
 </template>
 
 <style scoped>
-
+  header {
+    height: 3rem;
+    width: 100%;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0 1rem;
+  }
 </style>
