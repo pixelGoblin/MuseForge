789c51cbc6ecede7fecb4b91805b089fd6388892
Sartre666 <delphi100500@gmail.com>
2025-03-15 18:33:42 +0300

    init: initialize the project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import KeywordFields from './components/KeywordFields.vue'
+import InspirationCards from './components/InspirationCards.vue'
+</script>
+
+<template>
+  <header>
+
+  </header>
+
+  <main>
+    <KeywordFields />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
