b8c81afd2db20055c128319a7909af00c77f04af
Sartre666 <delphi100500@gmail.com>
2025-03-16 03:28:32 +0300

    refactor: add icons and fonts

--- src/main.js
+++ src/main.js
@@ -1,11 +1,31 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
+import '@mdi/font/css/materialdesignicons.css'
+
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
 const app = createApp(App)
 
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+})
+app.use(vuetify)
 app.use(createPinia())
 
 app.mount('#app')
